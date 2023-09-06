10537794f5a9be7659a94715a0e9ca1ed9716378
Liplum <Li_plum@outlook.com>
2023-09-07 01:13:38 +0800

    l10n for navigation bar.

--- lib/main/index.dart
+++ lib/main/index.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mimir/timetable/i18n.dart' as $timetable;
+import 'package:mimir/school/i18n.dart' as $school;
+import 'package:mimir/life/i18n.dart' as $life;
+import 'package:mimir/me/i18n.dart' as $me;
 
 class MainStagePage extends StatefulWidget {
   final StatefulNavigationShell navigationShell;
@@ -14,40 +17,50 @@ class MainStagePage extends StatefulWidget {
 class _MainStagePageState extends State<MainStagePage> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   var currentStage = 0;
-  late final List<({String route, BottomNavigationBarItem item})> items = [
-    (
-      route: "/timetable",
-      item: BottomNavigationBarItem(
-        icon: Icon(Icons.calendar_month_outlined),
-        activeIcon: Icon(Icons.calendar_month),
-        label: $timetable.i18n.navigation,
-      )
-    ),
-    (
-      route: "/school",
-      item: BottomNavigationBarItem(
-        icon: Icon(Icons.school_outlined),
-        activeIcon: Icon(Icons.school),
-        label: "School",
-      )
-    ),
-    (
-      route: "/life",
-      item: BottomNavigationBarItem(
-        icon: Icon(Icons.spa_outlined),
-        activeIcon: Icon(Icons.spa),
-        label: "Life",
-      )
-    ),
-    (
-      route: "/me",
-      item: BottomNavigationBarItem(
-        icon: Icon(Icons.person_outline),
-        activeIcon: Icon(Icons.person),
-        label: "Me",
-      )
-    ),
-  ];
+  late var items = buildItems();
+
+  List<({String route, BottomNavigationBarItem item})> buildItems() {
+    return [
+      (
+        route: "/timetable",
+        item: BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_month_outlined),
+          activeIcon: Icon(Icons.calendar_month),
+          label: $timetable.i18n.navigation,
+        )
+      ),
+      (
+        route: "/school",
+        item: BottomNavigationBarItem(
+          icon: Icon(Icons.school_outlined),
+          activeIcon: Icon(Icons.school),
+          label: $school.i18n.navigation,
+        )
+      ),
+      (
+        route: "/life",
+        item: BottomNavigationBarItem(
+          icon: Icon(Icons.spa_outlined),
+          activeIcon: Icon(Icons.spa),
+          label: $life.i18n.navigation,
+        )
+      ),
+      (
+        route: "/me",
+        item: BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          activeIcon: Icon(Icons.person),
+          label: $me.i18n.navigation,
+        )
+      ),
+    ];
+  }
+
+  @override
+  void didChangeDependencies() {
+    items = buildItems();
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
