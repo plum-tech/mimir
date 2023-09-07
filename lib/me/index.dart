55b628b0a693f005d6b915c1ef9d6a8b5a7e1f9e
Liplum <Li_plum@outlook.com>
2023-09-07 22:25:07 +0800

    group invitation button

--- lib/me/index.dart
+++ lib/me/index.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mimir/design/widgets/dialog.dart';
 import 'package:rettulf/rettulf.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+const _qGroupNumber = "917740212";
+const _joinQGroupUri =
+    "mqqapi://card/show_pslcard?src_type=internal&version=1&uin=$_qGroupNumber&card_type=group&source=qrcode";
 
 class MePage extends StatefulWidget {
   const MePage({super.key});
@@ -30,7 +37,33 @@ class _MePageState extends State<MePage> {
               },
             ),
           ],
-        )
+        ),
+        SliverList(
+            delegate: SliverChildListDelegate([
+          ListTile(
+            title: "预览版 QQ交流群".text(),
+            subtitle: _qGroupNumber.text(),
+            trailing: [
+              IconButton(
+                onPressed: () async {
+                  try {
+                    await launchUrlString(_joinQGroupUri);
+                  } catch (_) {}
+                },
+                icon: const Icon(Icons.group),
+              ),
+              IconButton(
+                onPressed: () async {
+                  await Clipboard.setData(const ClipboardData(text: _qGroupNumber));
+                  if (!mounted) return;
+                  context.showSnackBar("已复制到剪贴板".text());
+                },
+                icon: const Icon(Icons.copy),
+              ),
+            ].row(mas: MainAxisSize.min),
+            onTap: () {},
+          )
+        ]))
       ],
     );
   }
