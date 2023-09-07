f574b6c97dcd6f0421ba33bf5b5fc2840554eb93
Liplum <Li_plum@outlook.com>
2023-09-07 08:55:56 +0800

    changed i18n namespace

--- lib/life/expense/i18n.dart
+++ lib/life/expense/i18n.dart
@@ -6,7 +6,7 @@ const i18n = _I18n();
 class _I18n with CommonI18nMixin {
   const _I18n();
 
-  static const ns = "expense";
+  static const ns = "expense-record";
   final navigation = const _Navigation();
   final unit = const UnitI18n();
 
