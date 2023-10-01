576cb48c5a5b1a35943ea938e9fc0d6862da56e1
Liplum <Li_plum@outlook.com>
2023-10-01 08:20:10 +0800

    renaming

--- lib/credential/storage/credential.dart
+++ lib/credential/storage/credential.dart
@@ -5,14 +5,14 @@ import '../entity/credential.dart';
 import '../entity/email.dart';
 import '../entity/login_status.dart';
 
-class _OaKey {
+class _OaK {
   static const ns = "/oa";
   static const credentials = "$ns/credentials";
   static const lastAuthTime = "$ns/lastAuthTime";
   static const loginStatus = "$ns/loginStatus";
 }
 
-class _EmailKey {
+class _EmailK {
   static const ns = "/eduEmail";
   static const credentials = "$ns/credentials";
 }
@@ -23,30 +23,30 @@ class CredentialStorage {
   CredentialStorage(this.box);
 
   // OA
-  OaCredentials? get oaCredentials => box.get(_OaKey.credentials);
+  OaCredentials? get oaCredentials => box.get(_OaK.credentials);
 
-  set oaCredentials(OaCredentials? newV) => box.put(_OaKey.credentials, newV);
+  set oaCredentials(OaCredentials? newV) => box.put(_OaK.credentials, newV);
 
-  DateTime? get oaLastAuthTime => box.get(_OaKey.lastAuthTime);
+  DateTime? get oaLastAuthTime => box.get(_OaK.lastAuthTime);
 
-  set oaLastAuthTime(DateTime? newV) => box.put(_OaKey.lastAuthTime, newV);
+  set oaLastAuthTime(DateTime? newV) => box.put(_OaK.lastAuthTime, newV);
 
-  LoginStatus? get oaLoginStatus => box.get(_OaKey.loginStatus);
+  LoginStatus? get oaLoginStatus => box.get(_OaK.loginStatus);
 
-  set oaLoginStatus(LoginStatus? newV) => box.put(_OaKey.loginStatus, newV);
+  set oaLoginStatus(LoginStatus? newV) => box.put(_OaK.loginStatus, newV);
 
   ValueListenable<Box> listenOaChange() => box.listenable(keys: [
-        _OaKey.credentials,
-        _OaKey.lastAuthTime,
-        _OaKey.loginStatus,
+        _OaK.credentials,
+        _OaK.lastAuthTime,
+        _OaK.loginStatus,
       ]);
 
   // Edu Email
-  EmailCredentials? get eduEmailCredentials => box.get(_EmailKey.credentials);
+  EmailCredentials? get eduEmailCredentials => box.get(_EmailK.credentials);
 
-  set eduEmailCredentials(EmailCredentials? newV) => box.put(_EmailKey.credentials, newV);
+  set eduEmailCredentials(EmailCredentials? newV) => box.put(_EmailK.credentials, newV);
 
   ValueListenable<Box> listenEduEmailChange() => box.listenable(keys: [
-        _EmailKey.credentials,
+        _EmailK.credentials,
       ]);
 }
