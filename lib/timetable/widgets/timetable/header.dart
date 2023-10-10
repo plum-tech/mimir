d15cd9fb3f5af640d84c348fe763b2a32ed442a1
Liplum <Li_plum@outlook.com>
2023-10-11 03:31:37 +0800

    refactor timetable header

--- lib/timetable/widgets/timetable/header.dart
+++ lib/timetable/widgets/timetable/header.dart
@@ -4,16 +4,12 @@ import 'package:rettulf/rettulf.dart';
 
 import '../../i18n.dart';
 
-class TimetableHeader extends StatefulWidget {
-  /// 当前显示的周次
+BorderSide getBorderSide(BuildContext ctx) => BorderSide(color: ctx.colorScheme.primary.withOpacity(0.4), width: 0.8);
+
+class TimetableHeader extends StatelessWidget {
   final int currentWeek;
-
-  /// 当前初始选中的日 (1-7)
   final int selectedDay;
-
-  /// 点击的回调
   final Function(int)? onDayTap;
-
   final DateTime startDate;
 
   const TimetableHeader({
@@ -25,57 +21,57 @@ class TimetableHeader extends StatefulWidget {
   });
 
   @override
-  State<StatefulWidget> createState() => _TimetableHeaderState();
-}
-
-class _TimetableHeaderState extends State<TimetableHeader> {
-  int get selectedDay => widget.selectedDay;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  /// 布局标题栏下方的时间选择行
-  ///
-  /// 将该行分为 2 + 7 * 3 一共 23 个小份, 左侧的周数占 2 份, 每天的日期占 3 份.
-  @override
   Widget build(BuildContext context) {
+    final onDayTap = this.onDayTap;
     return [
-      for (int i = 1; i <= 7; ++i) buildDayNameHeader(i),
+      for (int dayIndex = 0; dayIndex < 7; dayIndex++)
+        Expanded(
+          flex: 3,
+          child: InkWell(
+            onTap: onDayTap != null
+                ? () {
+                    onDayTap.call(dayIndex + 1);
+                  }
+                : null,
+            child: HeaderCell(
+              weekIndex: currentWeek - 1,
+              dayIndex: dayIndex,
+              startDate: startDate,
+              backgroundColor: dayIndex + 1 == selectedDay ? context.colorScheme.secondaryContainer : null,
+            ),
+          ),
+        ),
     ].row(maa: MainAxisAlignment.spaceEvenly);
   }
+}
 
-  ///每天的列
-  Widget buildDayNameHeader(int day) {
-    final date = reflectWeekDayIndexToDate(weekIndex: widget.currentWeek, dayIndex: day, startDate: widget.startDate);
-    final onDayTap = widget.onDayTap;
-    return Expanded(
-      flex: 3,
-      child: InkWell(
-        onTap: onDayTap != null
-            ? () {
-                widget.onDayTap?.call(day);
-              }
-            : null,
-        child: buildDayHeader(day),
-      ),
-    );
-  }
+class HeaderCell extends StatelessWidget {
+  final int weekIndex;
+  final int dayIndex;
+  final DateTime startDate;
+  final Color? backgroundColor;
 
-  Widget buildDayHeader(int day) {
-    final date = reflectWeekDayIndexToDate(weekIndex: widget.currentWeek, dayIndex: day, startDate: widget.startDate);
-    final isSelected = day == selectedDay;
+  const HeaderCell({
+    super.key,
+    required this.weekIndex,
+    required this.dayIndex,
+    required this.startDate,
+    this.backgroundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final date = reflectWeekDayIndexToDate(weekIndex: weekIndex, dayIndex: dayIndex, startDate: startDate);
     final side = getBorderSide(context);
     return AnimatedContainer(
       decoration: BoxDecoration(
-        color: isSelected ? context.colorScheme.secondaryContainer : null,
+        color: backgroundColor,
         border: Border(bottom: side),
       ),
       duration: const Duration(milliseconds: 1000),
       curve: Curves.fastLinearToSlowEaseIn,
       child: [
-        i18n.weekdayShort(index: day - 1).text(
+        i18n.weekdayShort(index: dayIndex).text(
               textAlign: TextAlign.center,
               style: context.textTheme.titleSmall,
             ),
@@ -87,5 +83,3 @@ class _TimetableHeaderState extends State<TimetableHeader> {
     );
   }
 }
-
-BorderSide getBorderSide(BuildContext ctx) => BorderSide(color: ctx.colorScheme.primary.withOpacity(0.4), width: 0.8);
