16fb4798a1d17b753fe2d8e90b0211f84f5be8a4
Amazefcc233 <fcc_me@outlook.com>
2023-10-18 15:24:26 +0800

    fix: 版本号自增

--- ios-build-tools/addBuildNumber.py
+++ ios-build-tools/addBuildNumber.py
@@ -1,39 +1,24 @@
 import re
 
-# 实际显示版本号
-version = '1.0.0'
-# 实际构建版本号。默认情况时，每次构建时，都会自动+1。如果需要手动指定构建版本号，请将0修改为所需修改的值。
-# 务必注意，App Store上架时，构建版本号必须大于上一次上架的构建版本号。
-buildNumber = 0
-
-# 读取project.pbxproj文件，查看CURRENT_PROJECT_VERSION的值，然后+1，再写回去
-with open('ios/Runner.xcodeproj/project.pbxproj', 'r') as file:
+# 从pubspec.yaml文件中读取版本号，格式为x.x.x+x
+with open('pubspec.yaml', 'r') as file:
     filedata = file.read()
-
-if buildNumber == 0:
-    # 获取当前构建号
-    currentBuildNumber = re.findall(r'CURRENT_PROJECT_VERSION = (\d+);', filedata)[0]
-    print('oldBuildNumber: ' + currentBuildNumber)
-    currentBuildNumber = int(currentBuildNumber)
-    currentBuildNumber += 1
+version = re.findall(r'version: (\d+.\d+.\d+)', filedata)[0]
+oldBuildNumber = re.findall(r'version: \d+.\d+.\d+(\+\d+)', filedata)
+if len(oldBuildNumber) == 0:
+    buildNumber = 0
 else:
-    print('(user defined)BuildNumber: ' + str(buildNumber))
-    currentBuildNumber = buildNumber
+    buildNumber = int(oldBuildNumber[0][1:])
+    oldBuildNumber = buildNumber
 
-# 替换版本号与构建版本号
-filedata = re.sub(r'MARKETING_VERSION = (\d+.\d+.\d+);', 'MARKETING_VERSION = ' + version + ';', filedata)
-filedata = re.sub(r'CURRENT_PROJECT_VERSION = (\d+);', 'CURRENT_PROJECT_VERSION = ' + str(currentBuildNumber) + ';', filedata)
+# 版本号加1
+buildNumber += 1
+oldVersion = version + '+' + str(oldBuildNumber)
+newVersion = version + '+' + str(buildNumber)
+print('newVersion: ' + newVersion)
+print('buildNumber: ' + str(oldBuildNumber) + ' -> ' + str(buildNumber))
 
-print('currentBuildNumber: ' + str(version))
-print('currentBuildNumber: ' + str(currentBuildNumber))
-# 文件替换完成后，写回到文件中
-with open('ios/Runner.xcodeproj/project.pbxproj', 'w') as file:
+# 将新的版本号写入pubspec.yaml文件
+filedata = filedata.replace(f'version: ' + oldVersion, 'version: ' + newVersion)
+with open('pubspec.yaml', 'w') as file:
     file.write(filedata)
-
-# 如果buildNumber不为0，则修改此文件中的buildNumber变量值为0，并保存。
-if buildNumber != 0:
-    with open('ios-build-tools/addBuildNumber.py', 'r') as file:
-        filedata = file.read()
-    filedata = re.sub(r'buildNumber = (\d+)', 'buildNumber = 0', filedata)
-    with open('ios-build-tools/addBuildNumber.py', 'w') as file:
-        file.write(filedata)
