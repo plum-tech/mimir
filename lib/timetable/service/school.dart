b9224222dd65c6a0ec405d3dd86af9280a386af4
Wu <2321141691@qq.com>
2023-10-30 23:19:35 +0800

    Changes

--- lib/timetable/service/school.dart
+++ lib/timetable/service/school.dart
@@ -3,6 +3,7 @@ import 'package:sit/network/session.dart';
 import 'package:sit/school/entity/school.dart';
 import 'package:sit/session/gms.dart';
 import 'package:sit/session/jwxt.dart';
+import 'package:html/parser.dart';
 
 import '../entity/course.dart';
 import '../entity/timetable.dart';
@@ -43,6 +44,73 @@ class TimetableService {
       _postgraduateTimetableUrl,
       ReqMethod.get,
     );
+    if (response.statusCode == 200) {
+      final htmlContent = response.data;
+      final courseList = parseTimeTable(htmlContent);
+    }
+
     throw Exception("");
   }
+}
+
+class Course {
+  late String name;
+  late String time;
+  late String location;
+  late String teacher;
+  late String weekday;
+  late String code;
+}
+
+List parseTimeTable(String htmlContent) {
+  var courseList = [];
+  var mapOfWeekday = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'];
+
+  void processNodes(List nodes, String weekday) {
+    if (nodes.length < 5) {
+      // 如果节点数量小于 5，不足以构成一个完整的 Course，忽略
+      return;
+    }
+
+    var locationWithTeacherStr = nodes[4].text;
+    var locationWithTeacherList = locationWithTeacherStr.split("  ");
+    var location = locationWithTeacherList[0];
+    var teacher = locationWithTeacherList[1];
+
+    final course = Course()
+      ..name = nodes[0].text
+      ..time = nodes[2].text
+      ..location = location
+      ..teacher = teacher
+      ..weekday = weekday;
+
+    courseList.add(course);
+
+    // 移除处理过的节点，继续处理剩余的节点
+    nodes.removeRange(0, 7);
+
+    if (nodes.isNotEmpty) {
+      processNodes(nodes, weekday);
+    }
+  }
+
+  final document = parse(htmlContent);
+  final table = document.querySelector('table');
+  final trList = table?.querySelectorAll('tr');
+  for (var tr in trList!) {
+    final tdList = tr.querySelectorAll('td');
+    tdList.forEach((td) {
+      if (td.innerHtml.contains("br")) {
+        var index = tdList.indexOf(td);
+        var weekday;
+        if (tdList.length > 8)
+          weekday = mapOfWeekday[index - 2];
+        else
+          weekday = mapOfWeekday[index - 1];
+        var nodes = td.nodes;
+        processNodes(nodes, weekday);
+      }
+    });
+  }
+  return courseList;
 }
