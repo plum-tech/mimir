0fd752e611351fc345c8c1a631b0cafaf234e11c
Liplum <Li_plum@outlook.com>
2023-10-31 17:26:16 +0800

    try login gms

--- lib/session/gms.dart
+++ lib/session/gms.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:sit/credentials/init.dart';
 import 'package:sit/exception/session.dart';
+import 'package:sit/init.dart';
 import 'package:sit/network/session.dart';
 import 'package:sit/session/sso.dart';
 
@@ -22,6 +23,7 @@ class GmsSession {
   }) async {
     options ??= SessionOptions();
     options.contentType = 'application/x-www-form-urlencoded;charset=utf-8';
+
     Future<Response> fetch() async {
       return await ssoSession.request(
         url,
@@ -35,7 +37,7 @@ class GmsSession {
     }
     final response = await fetch();
     final content = response.data;
-    if (content is String && content.contains("登录") == true) {
+    if (content is String && content.contains("正在登录") == true) {
       final credential = CredentialInit.storage.oaCredentials;
       if (credential == null) {
         throw LoginRequiredException(url: url);
