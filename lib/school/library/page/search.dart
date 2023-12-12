0220a819b425472f484b26ca9053415112bd9436
Liplum <Li_plum@outlook.com>
2023-12-12 23:02:45 +0800

    [library] filter empty search query in search history

--- lib/school/library/page/search.dart
+++ lib/school/library/page/search.dart
@@ -27,7 +27,7 @@ class LibrarySearchDelegate extends SearchDelegate<String> {
     if (searchMethod != null) {
       $searchMethod.value = searchMethod;
     }
-    query = keyword;
+    query = keyword.trim();
 
     showSuggestions(context);
     await Future.delayed(const Duration(milliseconds: 500));
@@ -73,6 +73,8 @@ class LibrarySearchDelegate extends SearchDelegate<String> {
   }
 
   Future<void> addHistory(String keyword, SearchMethod searchMethod) async {
+    keyword = keyword.trim();
+    if (keyword.isEmpty) return;
     await LibraryInit.searchStorage.addSearchHistory(SearchHistoryItem(
       keyword: keyword,
       searchMethod: searchMethod,
