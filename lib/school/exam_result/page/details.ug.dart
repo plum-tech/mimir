2c3b9aea36509c646fdf8ade432d505af53d1a10
Liplum <Li_plum@outlook.com>
2023-12-14 02:36:36 +0800

    [exam result] details page

--- lib/school/exam_result/page/details.ug.dart
+++ lib/school/exam_result/page/details.ug.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:rettulf/rettulf.dart';
+import 'package:sit/design/widgets/list_tile.dart';
+import 'package:sit/design/widgets/navigation.dart';
 import 'package:sit/school/exam_result/entity/result.ug.dart';
+import '../i18n.dart';
 
 class ExamResultUgDetailsPage extends StatefulWidget {
   final ExamResultUg result;
@@ -13,6 +17,42 @@ class ExamResultUgDetailsPage extends StatefulWidget {
 class _ExamResultDetailsPageState extends State<ExamResultUgDetailsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final result = widget.result;
+    final score = result.score;
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar.medium(
+            pinned: true,
+            title: result.courseName.text(),
+          ),
+          SliverList.list(children: [
+            if (score != null)
+              DetailListTile(
+                leading: const Icon(Icons.score),
+                title: "Score",
+                subtitle: result.score.toString(),
+              )
+            else
+              PageNavigationTile(
+                icon: const Icon(Icons.warning),
+                title: i18n.lessonNotEvaluated.text(),
+                subtitle: "Score is available after evaluation".text(), path: '/teacher-eval',
+              ),
+            if (result.teachers.isNotEmpty)
+              DetailListTile(
+                leading: Icon(result.teachers.length > 1 ? Icons.people : Icons.person),
+                title: "Teachers", // plural
+                subtitle: result.teachers.join(", "),
+              ),
+            DetailListTile(
+              leading: const Icon(Icons.school),
+              title: i18n.credit,
+              subtitle: result.credit.toString(),
+            ),
+          ]),
+        ],
+      ),
+    );
   }
 }
