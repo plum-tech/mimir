cc15ed8b191828dadb60c8d799dc5a579316385c
Liplum <Li_plum@outlook.com>
2023-12-28 18:46:29 +0800

    [timetable] FIXME in settings page

--- lib/settings/page/timetable.dart
+++ lib/settings/page/timetable.dart
@@ -29,10 +29,11 @@ class _TimetableSettingsPageState extends State<TimetableSettingsPage> {
           SliverList.list(
             children: [
               buildAutoUseImportedToggle(),
-              const Divider(),
-              buildCellStyle(),
-              buildP13n(),
-              buildBackground(),
+              // const Divider(),
+              // FIXME: https://github.com/flutter/flutter/issues/140586
+              // buildCellStyle(),
+              // buildP13n(),
+              // buildBackground(),
             ],
           ),
         ],
