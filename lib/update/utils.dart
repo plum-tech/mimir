1242dd1806aefe02a32e9f3b5d31ecd2b058dc80
Liplum <Li_plum@outlook.com>
2024-01-03 11:11:43 +0800

    [update] print error for debugging

--- lib/update/utils.dart
+++ lib/update/utils.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:sit/design/adaptive/foundation.dart';
 import 'package:sit/r.dart';
+import 'package:sit/utils/error.dart';
 import 'package:universal_platform/universal_platform.dart';
 
 import 'init.dart';
@@ -12,12 +13,16 @@ Future<void> checkAppUpdate({
   Duration delayAtLeast = Duration.zero,
 }) async {
   if (UniversalPlatform.isIOS || UniversalPlatform.isMacOS) return;
-  final (latest, _) = await (UpdateInit.service.getLatestVersion(), Future.delayed(delayAtLeast)).wait;
-  debugPrint(latest.toString());
-  if (latest.downloadOf(R.currentVersion.platform) == null) return;
-  final canUpdate = latest.version > R.currentVersion.version;
-  if (!context.mounted) return;
-  if (canUpdate || kDebugMode) {
-    await context.show$Sheet$((ctx) => ArtifactUpdatePage(info: latest));
+  try {
+    final (latest, _) = await (UpdateInit.service.getLatestVersion(), Future.delayed(delayAtLeast)).wait;
+    debugPrint(latest.toString());
+    if (latest.downloadOf(R.currentVersion.platform) == null) return;
+    final canUpdate = latest.version > R.currentVersion.version;
+    if (!context.mounted) return;
+    if (canUpdate || kDebugMode) {
+      await context.show$Sheet$((ctx) => ArtifactUpdatePage(info: latest));
+    }
+  } catch (error, stackTrace) {
+    debugPrintError(error, stackTrace);
   }
 }
--- lib/utils/error.dart
+++ lib/utils/error.dart
@@ -1,10 +1,24 @@
+import 'dart:async';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 
-void debugPrintError(Object error, [StackTrace? stackTrace]) {
-  if (error is DioException) {
+void debugPrintError(Object? error, [StackTrace? stackTrace]) {
+  if (error == null) {
+    return;
+  } else if (error is DioException) {
     debugPrint(error.toString());
     debugPrintStack(stackTrace: error.stackTrace, maxFrames: 10);
+  } else if (error is AsyncError) {
+    debugPrintError(error.error, error.stackTrace);
+  } else if (error is ParallelWaitError) {
+    final errors = error.errors;
+    if (errors is (AsyncError?, AsyncError?)) {
+      debugPrintError(errors.$1);
+      debugPrintError(errors.$2);
+    } else {
+      debugPrint(errors.toString());
+    }
   } else {
     debugPrint(error.toString());
     debugPrintStack(stackTrace: stackTrace);
