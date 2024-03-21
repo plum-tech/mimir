85bbdfed2a41c563e1f5c18563984f06b286efbe
Liplum <Li_plum@outlook.com>
2024-03-21 13:21:31 +0800

    quick actions

---
+++ lib/platform/quick_action.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:quick_actions/quick_actions.dart';
+import 'package:sit/route.dart';
+
+class _Type {
+  const _Type._();
+
+  static const examArrange = "exam_arrange";
+  static const oaAnnounce = "oa_announce";
+}
+
+class QuickAction {
+  static const QuickActions _quickActions = QuickActions();
+
+  static void quickActionHandler(String type) {
+    final ctx = $Key.currentContext;
+    if (ctx == null) return;
+    switch (type) {
+      case _Type.examArrange:
+        ctx.push("/exam_arrange");
+        break;
+      case _Type.oaAnnounce:
+        ctx.push("/oa_announce");
+        break;
+    }
+  }
+
+  static void init(BuildContext context) {
+    _quickActions.initialize(quickActionHandler);
+    _quickActions.setShortcutItems(<ShortcutItem>[
+      ShortcutItem(
+        type: _Type.examArrange,
+        localizedTitle: "Exam arrangement",
+      ),
+      ShortcutItem(
+        type: _Type.oaAnnounce,
+        localizedTitle: "OA announcement",
+      ),
+    ]);
+  }
+}
