f8b4e43501afd250b99bacff58c3dc873b1480be
Liplum <Li_plum@outlook.com>
2024-03-29 13:30:51 +0800

    [edu email] fixed wrong login error tip

--- lib/me/edu_email/service/email.dart
+++ lib/me/edu_email/service/email.dart
@@ -1,5 +1,6 @@
 import 'package:enough_mail/enough_mail.dart';
 import 'package:sit/credentials/entity/credential.dart';
+import 'package:sit/credentials/error.dart';
 
 const _server = "imap.mail.sit.edu.cn";
 const _port = 993;
@@ -8,8 +9,15 @@ class MailService {
   final ImapClient _client = ImapClient(isLogEnabled: true, onBadCertificate: (_) => true);
 
   Future<List<Capability>> login(Credentials credential) async {
-    await _client.connectToServer(_server, _port, isSecure: true);
-    return await _client.login(credential.account, credential.password);
+    try {
+      await _client.connectToServer(_server, _port, isSecure: true);
+      return await _client.login(credential.account, credential.password);
+    } catch (error) {
+      if (error is ImapException) {
+        throw CredentialsException(type: CredentialsErrorType.accountPassword, message: error.message);
+      }
+      rethrow;
+    }
   }
 
   Future<FetchImapResult> getInboxMessage([int count = 30]) async {
