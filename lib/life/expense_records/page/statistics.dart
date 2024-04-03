4e17eb80c0f064fe870a52ac9c994ef74667c40a
Liplum <Li_plum@outlook.com>
2024-04-03 13:04:49 +0800

    [expense] [statistics] riverpod

--- lib/life/expense_records/page/statistics.dart
+++ lib/life/expense_records/page/statistics.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sit/design/adaptive/multiplatform.dart';
 import 'package:sit/design/widgets/card.dart';
 import 'package:sit/life/expense_records/entity/statistics.dart';
@@ -14,135 +15,104 @@ import '../init.dart';
 import '../widget/chart.dart';
 import '../widget/transaction.dart';
 
-class ExpenseStatisticsPage extends StatefulWidget {
+class ExpenseStatisticsPage extends ConsumerStatefulWidget {
   const ExpenseStatisticsPage({super.key});
 
   @override
-  State<ExpenseStatisticsPage> createState() => _ExpenseStatisticsPageState();
+  ConsumerState<ExpenseStatisticsPage> createState() => _ExpenseStatisticsPageState();
 }
 
 typedef Type2transactions = Map<TransactionType, ({List<Transaction> records, double total, double proportion})>;
 
-class _ExpenseStatisticsPageState extends State<ExpenseStatisticsPage> {
-  late List<Transaction> allRecords;
-  var selectedMode = StatisticsMode.month;
-  late double total;
+final _allRecords = Provider.autoDispose((ref) {
+  final all = ExpenseRecordsInit.storage.getTransactionsByRange() ?? const [];
+  all.retainWhere((record) => record.type.isConsume);
+  return all;
+});
 
+final _statisticsMode =
+NotifierProvider.autoDispose<_StatisticsModeNotifier, StatisticsMode>(_StatisticsModeNotifier.new);
+
+class _StatisticsModeNotifier extends AutoDisposeNotifier<StatisticsMode> {
   @override
-  void initState() {
-    super.initState();
-    refreshRecords();
-  }
+  StatisticsMode build() => StatisticsMode.month;
 
-  void refreshRecords() {
-    allRecords = ExpenseRecordsInit.storage.getTransactionsByRange() ?? const [];
-    allRecords.retainWhere((record) => record.type.isConsume);
+  void set(StatisticsMode mode) {
+    state = mode;
   }
+}
 
-  Widget buildModeSelector() {
-    return SegmentedButton<StatisticsMode>(
-      showSelectedIcon: false,
-      segments: StatisticsMode.values
-          .map((e) => ButtonSegment<StatisticsMode>(
-                value: e,
-                label: e.l10nName().text(),
-              ))
-          .toList(),
-      selected: <StatisticsMode>{selectedMode},
-      onSelectionChanged: (newSelection) {
-        setState(() {
-          selectedMode = newSelection.first;
-        });
-      },
-    );
-  }
+final _startTime2Records = Provider.autoDispose((ref) {
+  final mode = ref.watch(_statisticsMode);
+  final all = ref.watch(_allRecords);
+  return mode.resort(all);
+});
 
+class _ExpenseStatisticsPageState extends ConsumerState<ExpenseStatisticsPage> {
   @override
   Widget build(BuildContext context) {
+    final mode = ref.watch(_statisticsMode);
     return Scaffold(
       appBar: AppBar(
         title: i18n.stats.title.text(),
       ),
       body: [
-        buildModeSelector().padSymmetric(h: 16, v: 4),
-        StatisticsSection(
-          mode: selectedMode,
-          all: allRecords,
-        ).expanded(),
+        buildModeSelector(mode).padSymmetric(h: 16, v: 4),
+        const StatisticsSection().expanded(),
       ].column(caa: CrossAxisAlignment.stretch),
     );
-    // final now = DateTime.now();
-    // final years = _getYear(records);
-    // final months = _getMonth(records, years, selectedYear);
-    // return Scaffold(
-    //   body: CustomScrollView(
-    //     slivers: [
-    //       SliverAppBar(
-    //         pinned: true,
-    //         expandedHeight: 200,
-    //         flexibleSpace: FlexibleSpaceBar(
-    //           title: i18n.stats.title.text(),
-    //           centerTitle: true,
-    //           background: YearMonthSelector(
-    //             years: years,
-    //             months: months,
-    //             initialYear: now.year,
-    //             initialMonth: now.month,
-    //           ),
-    //         ),
-    //       ),
-    //       SliverToBoxAdapter(
-    //         child: _buildChartView(),
-    //       ),
-    //       SliverToBoxAdapter(
-    //         child: ExpensePieChart(records: type2transactions),
-    //       ),
-    //     ],
-    //   ),
-    // );
+  }
+
+  Widget buildModeSelector(StatisticsMode selected) {
+    return SegmentedButton<StatisticsMode>(
+      showSelectedIcon: false,
+      segments: StatisticsMode.values
+          .map((e) =>
+          ButtonSegment<StatisticsMode>(
+            value: e,
+            label: e.l10nName().text(),
+          ))
+          .toList(),
+      selected: <StatisticsMode>{selected},
+      onSelectionChanged: (newSelection) {
+        ref.read(_statisticsMode.notifier).set(newSelection.first);
+      },
+    );
   }
 }
 
-class StatisticsSection extends StatefulWidget {
-  final StatisticsMode mode;
-  final List<Transaction> all;
-
+class StatisticsSection extends ConsumerStatefulWidget {
   const StatisticsSection({
     super.key,
-    required this.mode,
-    required this.all,
   });
 
   @override
-  State<StatisticsSection> createState() => _StatisticsSectionState();
+  ConsumerState<StatisticsSection> createState() => _StatisticsSectionState();
 }
 
-class _StatisticsSectionState extends State<StatisticsSection> {
-  late StartTime2Records startTime2Records = widget.mode.resort(widget.all);
-  late int index = startTime2Records.length - 1;
-
-  @override
-  void didUpdateWidget(covariant StatisticsSection oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.mode != widget.mode || oldWidget.all != widget.all) {
-      setState(() {
-        startTime2Records = widget.mode.resort(widget.all);
-        index = startTime2Records.length - 1;
-      });
-    }
-  }
+class _StatisticsSectionState extends ConsumerState<StatisticsSection> {
+  late int index = ref.read(_startTime2Records).length - 1;
 
   @override
   Widget build(BuildContext context) {
+    final startTime2Records = ref.watch(_startTime2Records);
+    final mode = ref.watch(_statisticsMode);
+    ref.listen(_startTime2Records, (previous, next) {
+      setState(() {
+        index = next.length - 1;
+      });
+    });
     final current = startTime2Records.indexAt(index);
     final separated = separateTransactionByType(current.records);
     return [
       [
-        SizedBox(height: 50,),
+        SizedBox(
+          height: 50,
+        ),
         ExpenseLineChart(
           start: current.start,
           records: current.records,
-          mode: widget.mode,
+          mode: mode,
         ),
         ExpensePieChart(records: separated),
         ...current.records.map((record) {
@@ -152,44 +122,31 @@ class _StatisticsSectionState extends State<StatisticsSection> {
       // ListView()
       buildHeader(current.start).align(at: Alignment.topCenter),
     ].stack();
-    return [
-      buildHeader(current.start),
-      [
-        ExpenseLineChart(
-          start: current.start,
-          records: current.records,
-          mode: widget.mode,
-        ),
-        ExpensePieChart(records: separated),
-        ...current.records.map((record) {
-          return TransactionTile(record);
-        }),
-      ].listview().expanded(),
-      // ListView()
-    ].column();
   }
 
   Widget buildHeader(DateTime start) {
+    final startTime2Records = ref.watch(_startTime2Records);
+    final mode = ref.watch(_statisticsMode);
     return OutlinedCard(
       child: [
         PlatformIconButton(
           onPressed: index > 0
               ? () {
-                  setState(() {
-                    index = index - 1;
-                  });
-                }
+            setState(() {
+              index = index - 1;
+            });
+          }
               : null,
           icon: Icon(context.icons.leftChevron),
         ),
-        resolveTime4Display(context: context, mode: widget.mode, date: start).text(),
+        resolveTime4Display(context: context, mode: mode, date: start).text(),
         PlatformIconButton(
           onPressed: index < startTime2Records.length - 1
               ? () {
-                  setState(() {
-                    index = index + 1;
-                  });
-                }
+            setState(() {
+              index = index + 1;
+            });
+          }
               : null,
           icon: Icon(context.icons.rightChevron),
         ),
