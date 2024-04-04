b66644bf3bf5820dd8615e3461ce218496eb9672
Liplum <Li_plum@outlook.com>
2024-04-05 03:51:50 +0800

    [expense] [statistics] StatisticsDelegate

--- lib/life/expense_records/widget/chart/bar.dart
+++ lib/life/expense_records/widget/chart/bar.dart
@@ -28,101 +28,157 @@ class ExpenseLineChart extends StatefulWidget {
   State<ExpenseLineChart> createState() => _ExpenseLineChartState();
 }
 
-final _monthFormat = DateFormat.MMM();
-
 class _ExpenseLineChartState extends State<ExpenseLineChart> {
   @override
   Widget build(BuildContext context) {
-    final (:data, :titles) = buildData(
+    final delegate = StatisticsDelegate.byMode(
+      widget.mode,
       start: widget.start,
-      mode: widget.mode,
       records: widget.records,
     );
     return OutlinedCard(
       child: AspectRatio(
         aspectRatio: 1.5,
         child: AmountChartWidget(
-          bottomTitles: titles,
-          values: data,
+          delegate: delegate,
         ).padSymmetric(v: 12, h: 8),
       ),
     );
   }
 }
 
-({List<double> data, List<String> titles}) buildData({
-  required DateTime start,
-  required StatisticsMode mode,
-  required List<Transaction> records,
-}) {
-  final now = DateTime.now();
-  switch (mode) {
-    case StatisticsMode.week:
-      final List<double> weekAmount = List.filled(
-        start.year == now.year && start.week == now.week ? now.weekday : 7,
-        0.00,
-      );
-      for (final record in records) {
-        // add data at the same weekday.
-        // sunday goes first
-        weekAmount[record.timestamp.weekday == DateTime.sunday ? 0 : record.timestamp.weekday] += record.deltaAmount;
-      }
-      return (data: weekAmount, titles: Weekday.calendarOrder.map((w) => w.l10nShort()).toList());
-    case StatisticsMode.month:
-      final List<double> dayAmount = List.filled(
-          start.year == now.year && start.month == now.month
-              ? now.day
-              : daysInMonth(year: start.year, month: start.month),
-          0.00);
-      for (final record in records) {
-        // add data on the same day.
-        dayAmount[record.timestamp.day - 1] += record.deltaAmount;
-      }
-      return (data: dayAmount, titles: List.generate(dayAmount.length, (i) => (i + 1).toString()));
-    case StatisticsMode.year:
-      final List<double> monthAmount = List.filled(start.year == now.year ? now.month : 12, 0.00);
-      for (final record in records) {
-        // add data in the same month.
-        monthAmount[record.timestamp.month - 1] += record.deltaAmount;
-      }
-      return (
-        data: monthAmount,
-        titles: List.generate(monthAmount.length, (i) => _monthFormat.format(DateTime(0, i + 1)).substring(0, 3))
-      );
-  }
-}
+final _monthFormat = DateFormat.MMM();
 
-class AmountChartWidget extends StatelessWidget {
-  final List<String> bottomTitles;
-  final List<double> values;
+class StatisticsDelegate {
+  final List<double> data;
+  final StatisticsMode mode;
+  final GetTitleWidgetFunction side;
+  final GetTitleWidgetFunction bottom;
 
-  const AmountChartWidget({
-    super.key,
-    required this.bottomTitles,
-    required this.values,
+  const StatisticsDelegate({
+    required this.mode,
+    required this.data,
+    required this.side,
+    required this.bottom,
   });
 
-  Widget buildLeftTitle(double value, TitleMeta meta) {
+  factory StatisticsDelegate.byMode(
+    StatisticsMode mode, {
+    required DateTime start,
+    required List<Transaction> records,
+  }) {
+    switch (mode) {
+      case StatisticsMode.week:
+        return StatisticsDelegate.week(start: start, records: records);
+      case StatisticsMode.month:
+        return StatisticsDelegate.month(start: start, records: records);
+      case StatisticsMode.year:
+        return StatisticsDelegate.year(start: start, records: records);
+    }
+  }
+
+  factory StatisticsDelegate.week({
+    required DateTime start,
+    required List<Transaction> records,
+  }) {
+    final now = DateTime.now();
+    final List<double> weekAmount = List.filled(
+      start.year == now.year && start.week == now.week ? now.weekday : 7,
+      0.00,
+    );
+    for (final record in records) {
+      // add data at the same weekday.
+      // sunday goes first
+      weekAmount[record.timestamp.weekday == DateTime.sunday ? 0 : record.timestamp.weekday] += record.deltaAmount;
+    }
+    return StatisticsDelegate(
+      mode: StatisticsMode.week,
+      data: weekAmount,
+      side: _buildSideTitle,
+      bottom: (value, mate) {
+        final index = value.toInt();
+        return SideTitleWidget(
+          axisSide: mate.axisSide,
+          child: Text(
+            Weekday.calendarOrder[index].l10nShort(),
+          ),
+        );
+      },
+    );
+  }
+
+  factory StatisticsDelegate.month({
+    required DateTime start,
+    required List<Transaction> records,
+  }) {
+    final now = DateTime.now();
+    final List<double> dayAmount = List.filled(
+        start.year == now.year && start.month == now.month
+            ? now.day
+            : daysInMonth(year: start.year, month: start.month),
+        0.00);
+    for (final record in records) {
+      // add data on the same day.
+      dayAmount[record.timestamp.day - 1] += record.deltaAmount;
+    }
+    return StatisticsDelegate(
+      mode: StatisticsMode.week,
+      data: dayAmount,
+      side: _buildSideTitle,
+      bottom: (value, mate) {
+        final index = value.toInt();
+        return SideTitleWidget(
+          axisSide: mate.axisSide,
+          child: Text(
+            "${index + 1}",
+          ),
+        );
+      },
+    );
+  }
+
+  factory StatisticsDelegate.year({
+    required DateTime start,
+    required List<Transaction> records,
+  }) {
+    final now = DateTime.now();
+    final List<double> monthAmount = List.filled(start.year == now.year ? now.month : 12, 0.00);
+    for (final record in records) {
+      // add data in the same month.
+      monthAmount[record.timestamp.month - 1] += record.deltaAmount;
+    }
+    return StatisticsDelegate(
+      mode: StatisticsMode.week,
+      data: monthAmount,
+      side: _buildSideTitle,
+      bottom: (value, mate) {
+        final index = value.toInt();
+        return SideTitleWidget(
+          axisSide: mate.axisSide,
+          child: Text(
+            _monthFormat.format(DateTime(0, index + 1)).substring(0, 3),
+          ),
+        );
+      },
+    );
+  }
+
+  static Widget _buildSideTitle(double value, TitleMeta meta) {
     String text = '¥${value.round()}';
     return SideTitleWidget(
       axisSide: meta.axisSide,
       child: Text(text),
     );
   }
+}
 
-  Widget buildBottomTitle(double value, TitleMeta mate) {
-    final index = value.toInt();
-    if (!(index == 0 || index == values.length - 1) && index % 5 != 0) {
-      return const SizedBox();
-    }
+class AmountChartWidget extends StatelessWidget {
+  final StatisticsDelegate delegate;
 
-    return SideTitleWidget(
-      axisSide: mate.axisSide,
-      child: Text(
-        bottomTitles[index],
-      ),
-    );
-  }
+  const AmountChartWidget({
+    super.key,
+    required this.delegate,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -136,14 +192,14 @@ class AmountChartWidget extends StatelessWidget {
             sideTitles: SideTitles(
               showTitles: true,
               reservedSize: 28,
-              getTitlesWidget: buildBottomTitle,
+              getTitlesWidget: delegate.bottom,
             ),
           ),
           leftTitles: AxisTitles(
             sideTitles: SideTitles(
               showTitles: true,
               reservedSize: 40,
-              getTitlesWidget: buildLeftTitle,
+              getTitlesWidget: delegate.side,
             ),
           ),
           topTitles: const AxisTitles(
@@ -166,7 +222,7 @@ class AmountChartWidget extends StatelessWidget {
           show: false,
         ),
         groupsSpace: 40,
-        barGroups: values
+        barGroups: delegate.data
             .mapIndexed((i, v) => BarChartGroupData(
                   x: i,
                   barRods: [
