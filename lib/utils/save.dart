8bf082f9e92d4e8b32e6eea557833dd5a685d6e4
Liplum <Li_plum@outlook.com>
2024-04-22 14:30:06 +0800

    PromptSaveBeforeQuitScope

--- lib/utils/save.dart
+++ lib/utils/save.dart
@@ -3,25 +3,55 @@ import 'dart:async';
 import 'package:flutter/widgets.dart';
 import 'package:sit/design/adaptive/dialog.dart';
 
-extension BuildContextPromptX on BuildContext {
-  void Function() promptSaveBeforeQuit({
-    required FutureOr<void> Function() save,
-    required FutureOr<void> Function() quit,
-  }) {
-    return () async {
-      final confirmSave = await showDialogRequest(
-        desc: 'You have unsaved changes, do you want to save them?',
-        yes: 'Save&Quit',
-        no: 'Abort',
-      );
-      if (confirmSave == true) {
-        await save();
-        await quit();
-      } else if (confirmSave == false) {
-        await quit();
-      } else {
-        // do nothing when dismiss
-      }
-    };
+class PromptSaveBeforeQuitScope extends StatefulWidget {
+  final bool canSave;
+  final FutureOr<void> Function() onSave;
+  final Widget child;
+
+  const PromptSaveBeforeQuitScope({
+    super.key,
+    required this.canSave,
+    required this.onSave,
+    required this.child,
+  });
+
+  @override
+  State<PromptSaveBeforeQuitScope> createState() => _PromptSaveBeforeQuitScopeState();
+}
+
+class _PromptSaveBeforeQuitScopeState extends State<PromptSaveBeforeQuitScope> {
+  var canPop = false;
+
+  @override
+  void didUpdateWidget(covariant PromptSaveBeforeQuitScope oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.canSave) {
+      setState(() {
+        canPop = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: canPop,
+      onPopInvoked: (didPop) async {
+        if (!didPop) return;
+        final confirmSave = await context.showDialogRequest(
+          desc: 'You have unsaved changes, do you want to save them?',
+          yes: 'Save&Quit',
+          no: 'Abort',
+        );
+        if (confirmSave == true) {
+          await widget.onSave();
+          if (!mounted) return;
+          setState(() {
+            canPop = true;
+          });
+        }
+      },
+      child: widget.child,
+    );
   }
 }
