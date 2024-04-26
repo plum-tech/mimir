6d00106ca644e794298688f89f3a16438978e10a
Liplum <Li_plum@outlook.com>
2024-04-26 15:21:44 +0800

    [timetable] builtin timetable patch `sitVacationShift2024`

--- lib/timetable/patch.dart
+++ lib/timetable/patch.dart
@@ -5,6 +5,16 @@ class BuiltinTimetablePatchSets {
   static final vacationShift2024 = BuiltinTimetablePatchSet(
     key: "sitVacationShift2024",
     patches: [
+      TimetableRemoveDayPatch(loc: TimetableDayLoc.date(DateTime(2024, 1, 1))),
+      TimetableRemoveDayPatch(loc: TimetableDayLoc.date(DateTime(2024, 4, 4))),
+      TimetableMoveDayPatch(
+        source: TimetableDayLoc.date(DateTime(2024, 4, 5)),
+        target: TimetableDayLoc.date(DateTime(2024, 4, 3)),
+      ),
+      TimetableMoveDayPatch(
+        source: TimetableDayLoc.date(DateTime(2024, 4, 3)),
+        target: TimetableDayLoc.date(DateTime(2024, 4, 7)),
+      ),
       TimetableMoveDayPatch(
         source: TimetableDayLoc.date(DateTime(2024, 4, 30)),
         target: TimetableDayLoc.date(DateTime(2024, 4, 28)),
@@ -17,6 +27,39 @@ class BuiltinTimetablePatchSets {
         source: TimetableDayLoc.date(DateTime(2024, 5, 10)),
         target: TimetableDayLoc.date(DateTime(2024, 5, 11)),
       ),
+      TimetableRemoveDayPatch(loc: TimetableDayLoc.date(DateTime(2024, 6, 10))),
+      TimetableMoveDayPatch(
+        source: TimetableDayLoc.date(DateTime(2024, 9, 16)),
+        target: TimetableDayLoc.date(DateTime(2024, 9, 13)),
+      ),
+      TimetableMoveDayPatch(
+        source: TimetableDayLoc.date(DateTime(2024, 9, 13)),
+        target: TimetableDayLoc.date(DateTime(2024, 9, 14)),
+      ),
+      TimetableRemoveDayPatch(loc: TimetableDayLoc.date(DateTime(2024, 9, 17))),
+      TimetableMoveDayPatch(
+        source: TimetableDayLoc.date(DateTime(2024, 9, 4)),
+        target: TimetableDayLoc.date(DateTime(2024, 9, 29)),
+      ),
+      TimetableMoveDayPatch(
+        source: TimetableDayLoc.date(DateTime(2024, 10, 4)),
+        target: TimetableDayLoc.date(DateTime(2024, 9, 30)),
+      ),
+      TimetableMoveDayPatch(
+        source: TimetableDayLoc.date(DateTime(2024, 10, 7)),
+        target: TimetableDayLoc.date(DateTime(2024, 10, 11)),
+      ),
+      TimetableMoveDayPatch(
+        source: TimetableDayLoc.date(DateTime(2024, 10, 11)),
+        target: TimetableDayLoc.date(DateTime(2024, 10, 12)),
+      ),
+      TimetableRemoveDayPatch(loc: TimetableDayLoc.date(DateTime(2024, 10, 1))),
+      TimetableRemoveDayPatch(loc: TimetableDayLoc.date(DateTime(2024, 10, 2))),
+      TimetableRemoveDayPatch(loc: TimetableDayLoc.date(DateTime(2024, 10, 3))),
+      TimetableRemoveDayPatch(loc: TimetableDayLoc.date(DateTime(2024, 10, 4))),
+      TimetableRemoveDayPatch(loc: TimetableDayLoc.date(DateTime(2024, 10, 5))),
+      TimetableRemoveDayPatch(loc: TimetableDayLoc.date(DateTime(2024, 10, 6))),
+      TimetableRemoveDayPatch(loc: TimetableDayLoc.date(DateTime(2024, 10, 7)))
     ],
   );
   static final all = [
