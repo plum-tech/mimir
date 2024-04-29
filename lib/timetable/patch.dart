8fc429d05100f666b908023c967aab48bd996c16
Liplum <Li_plum@outlook.com>
2024-04-29 23:51:33 +0800

    [timetable] fixed `vacationShift2024` patch v2

--- lib/timetable/patch.dart
+++ lib/timetable/patch.dart
@@ -10,12 +10,12 @@ class BuiltinTimetablePatchSets {
       // Qingming Festival
       TimetableRemoveDayPatch.oneDay(loc: TimetableDayLoc.byDate(2024, 4, 4)),
       TimetableMoveDayPatch(
-        source: TimetableDayLoc.byDate(2024, 4, 5),
-        target: TimetableDayLoc.byDate(2024, 4, 3),
-      ),
-      TimetableMoveDayPatch(
         source: TimetableDayLoc.byDate(2024, 4, 3),
         target: TimetableDayLoc.byDate(2024, 4, 7),
+      ),
+      TimetableMoveDayPatch(
+        source: TimetableDayLoc.byDate(2024, 4, 5),
+        target: TimetableDayLoc.byDate(2024, 4, 3),
       ),
       // International Workers' Day
       TimetableMoveDayPatch(
@@ -27,24 +27,24 @@ class BuiltinTimetablePatchSets {
         target: TimetableDayLoc.byDate(2024, 4, 30),
       ),
       TimetableMoveDayPatch(
-        source: TimetableDayLoc.byDate(2024, 5, 2),
-        target: TimetableDayLoc.byDate(2024, 5, 10),
-      ),
-      TimetableMoveDayPatch(
         source: TimetableDayLoc.byDate(2024, 5, 10),
         target: TimetableDayLoc.byDate(2024, 5, 11),
+      ),
+      TimetableMoveDayPatch(
+        source: TimetableDayLoc.byDate(2024, 5, 2),
+        target: TimetableDayLoc.byDate(2024, 5, 10),
       ),
       TimetableRemoveDayPatch.oneDay(loc: TimetableDayLoc.byDate(2024, 5, 1)),
       // Dragon Boat Festival
       TimetableRemoveDayPatch.oneDay(loc: TimetableDayLoc.byDate(2024, 6, 10)),
       // Mid-Autumn Festival
       TimetableMoveDayPatch(
-        source: TimetableDayLoc.byDate(2024, 9, 16),
-        target: TimetableDayLoc.byDate(2024, 9, 13),
-      ),
-      TimetableMoveDayPatch(
         source: TimetableDayLoc.byDate(2024, 9, 13),
         target: TimetableDayLoc.byDate(2024, 9, 14),
+      ),
+      TimetableMoveDayPatch(
+        source: TimetableDayLoc.byDate(2024, 9, 16),
+        target: TimetableDayLoc.byDate(2024, 9, 13),
       ),
       TimetableRemoveDayPatch.oneDay(loc: TimetableDayLoc.byDate(2024, 9, 17)),
       // the National Day of the PRC
@@ -57,12 +57,12 @@ class BuiltinTimetablePatchSets {
         target: TimetableDayLoc.byDate(2024, 9, 30),
       ),
       TimetableMoveDayPatch(
-        source: TimetableDayLoc.byDate(2024, 10, 7),
-        target: TimetableDayLoc.byDate(2024, 10, 11),
-      ),
-      TimetableMoveDayPatch(
         source: TimetableDayLoc.byDate(2024, 10, 11),
         target: TimetableDayLoc.byDate(2024, 10, 12),
+      ),
+      TimetableMoveDayPatch(
+        source: TimetableDayLoc.byDate(2024, 10, 7),
+        target: TimetableDayLoc.byDate(2024, 10, 11),
       ),
       TimetableRemoveDayPatch(all: [
         TimetableDayLoc.byDate(2024, 10, 1),
