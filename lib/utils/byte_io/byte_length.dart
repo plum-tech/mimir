026311ebd2b07f1761c5574935bee3407951a497
Liplum <Li_plum@outlook.com>
2024-05-02 08:31:06 +0800

    fixed value of bit64 in byte_io library

--- lib/utils/byte_io/byte_length.dart
+++ lib/utils/byte_io/byte_length.dart
@@ -1,6 +1,6 @@
 class ByteLengths {
-  static const bit8 = 2 ^ 8;
-  static const bit16 = 2 ^ 16;
-  static const bit32 = 2 ^ 32;
-  static const bit64 = 2 ^ 64;
+  static const bit8 = 0xFF;
+  static const bit16 = 0xFFFF;
+  static const bit32 = 0xFFFFFFFF;
+  static const bit64 = 0xFFFFFFFFFFFFFFFF;
 }
