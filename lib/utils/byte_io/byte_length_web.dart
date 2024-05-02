5fd88971b20029a6a0f91cee8c16291bd02c1b9e
Liplum <Li_plum@outlook.com>
2024-05-02 13:16:34 +0800

    [web] fixed ByteLengths.bit64 for web

--- lib/utils/byte_io/byte_length_web.dart
+++ lib/utils/byte_io/byte_length_web.dart
@@ -2,5 +2,5 @@ class ByteLengths {
   static const bit8 = 0xFF;
   static const bit16 = 0xFFFF;
   static const bit32 = 0xFFFFFFFF;
-  static const bit64 = 0xFFFFFFFFFFFFFFF;
+  static const bit64 = 0x100000000000000;
 }
