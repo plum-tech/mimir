f7a6d0c35e0052b065a30feb761d0a37dded12fd
Liplum <Li_plum@outlook.com>
2024-05-08 05:16:36 +0800

    [minesweeper] RecordMinesweeper

--- lib/game/minesweeper/entity/record.dart
+++ lib/game/minesweeper/entity/record.dart
@@ -1,7 +1,28 @@
+import 'package:json_annotation/json_annotation.dart';
 import 'package:sit/game/record/record.dart';
 
+import 'mode.dart';
+
+part "record.g.dart";
+
+@JsonSerializable()
 class RecordMinesweeper extends GameRecord {
+  final int rows;
+  final int columns;
+  final int mines;
+  final Duration playTime;
+  final GameMode mode;
+
   const RecordMinesweeper({
     required super.ts,
+    required this.rows,
+    required this.columns,
+    required this.mines,
+    required this.playTime,
+    required this.mode,
   });
+
+  Map<String, dynamic> toJson() => _$RecordMinesweeperToJson(this);
+
+  factory RecordMinesweeper.fromJson(Map<String, dynamic> json) => _$RecordMinesweeperFromJson(json);
 }
---
+++ lib/game/minesweeper/entity/record.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'record.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+RecordMinesweeper _$RecordMinesweeperFromJson(Map<String, dynamic> json) => RecordMinesweeper(
+      ts: DateTime.parse(json['ts'] as String),
+      rows: (json['rows'] as num).toInt(),
+      columns: (json['columns'] as num).toInt(),
+      mines: (json['mines'] as num).toInt(),
+      playTime: Duration(microseconds: (json['playTime'] as num).toInt()),
+      mode: GameMode.fromJson(json['mode'] as String),
+    );
+
+Map<String, dynamic> _$RecordMinesweeperToJson(RecordMinesweeper instance) => <String, dynamic>{
+      'ts': instance.ts.toIso8601String(),
+      'rows': instance.rows,
+      'columns': instance.columns,
+      'mines': instance.mines,
+      'playTime': instance.playTime.inMicroseconds,
+      'mode': instance.mode,
+    };
