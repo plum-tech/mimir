42a5b823c957cd991928b8a5e183836b44ff2882
Liplum <Li_plum@outlook.com>
2024-05-11 16:25:14 +0800

    [sudoku] added license

--- lib/game/minesweeper/page/game.dart
+++ lib/game/minesweeper/page/game.dart
@@ -1,3 +1,7 @@
+// Thanks to "https://github.com/einsitang/sudoku-flutter"
+// LICENSE: https://github.com/einsitang/sudoku-flutter/blob/fc31c063d84ba272bf30219ea08724272167b8ef/LICENSE
+// Modifications copyright©️2023–2024 Liplum Dev.
+
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
