2435c0e947a912c8caec9b6676f16236b1c47b59
Liplum <Li_plum@outlook.com>
2024-05-14 01:08:36 +0800

    [tools] remove test.mjs

--- tools/test.mjs
+++
@@ -1,9 +0,0 @@
-import { git } from "./git.mjs"
-async function main() {
-  console.log(`v{newVersion}
-  run id: {runId}
-  run_attempt(should be 1): {runAttempt}
-  {serverUrl}/{repository}/actions/runs/{runId}`)
-}
-
-main()
