7b85a35bf9a67da2b1ad28caa19fa7beeb1aa284
Liplum <Li_plum@outlook.com>
2024-05-14 17:28:50 +0800

    [tools] opt-in new sitmc API

--- tools/sitmc.mjs
+++ tools/sitmc.mjs
@@ -1,18 +1,39 @@
 import { readFile } from "fs/promises"
 import * as path from "path"
+import mime from 'mime'
+
+const prodUrl = "http://mua.sitmc.club:32000/admin"
 /**
  *
  * @param {{auth:string,filePath:string, uploadPath:string}} param0
  */
-export async function uploadFile({ auth, filePath, uploadPath }) {
+export async function uploadFile({ auth, localFilePath, remotePath }) {
   const formData = new FormData()
 
-  const file = new Blob([await readFile(filePath)])
-  formData.append('file', file, path.basename(filePath))
-  formData.append('path', uploadPath)
+  const file = new Blob([await readFile(localFilePath)], { type: mime.getType(localFilePath) })
+  formData.append('file', file, path.basename(localFilePath))
+  formData.append('path', remotePath)
 
-  const res = await fetch('http://mua.sitmc.club:32000/upload', {
-    method: 'POST',
+  const res = await fetch(prodUrl, {
+    method: 'PUT',
+    headers: {
+      Authorization: auth,
+    },
+    body: formData
+  })
+  const result = await res.json()
+  return result
+}
+/**
+ *
+ * @param {{auth:string,deletePath:string}} param0
+ */
+export async function deleteFile({ auth, remotePath }) {
+  const formData = new FormData()
+  formData.append('path', remotePath)
+
+  const res = await fetch(prodUrl, {
+    method: 'DELETE',
     headers: {
       Authorization: auth,
     },
