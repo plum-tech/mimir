0f5df089468dd513cea92813e835bdf77f14d401
Liplum <Li_plum@outlook.com>
2024-05-16 07:53:03 +0800

    [workflow] upload files onto sitmc temp server

--- tools/upload-sitmc.mjs
+++ tools/upload-sitmc.mjs
@@ -2,7 +2,7 @@ import { uploadFile } from "./sitmc.mjs"
 import * as path from "path"
 
 async function main() {
-  const auth = process.env.SITMC_UPLOAD_TOKEN
+  const auth = process.env.SITMC_TEMP_SERVER_AUTH
   const filePath = process.argv[2]
   const remotePath = process.argv[3] ?? path.join(path.basename(path.dirname(filePath)), path.basename(filePath))
   const res = await uploadFile({
