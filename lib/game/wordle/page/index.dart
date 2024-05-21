4ce438939cea8ead863310e9df6a832d1e0756d8
Liplum <Li_plum@outlook.com>
2024-05-22 01:14:24 +0800

    [wordle] remove unused code

--- lib/game/wordle/page/index.dart
+++ lib/game/wordle/page/index.dart
@@ -1,14 +1,9 @@
 // Thanks to https://github.com/Linloir/Flutter-Wordle
 
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../event_bus.dart';
 import '../page/loading.dart';
-import '../entity/vocabulary.dart';
 import '../settings.dart';
-import '../widget/selection_group.dart';
 
 class GameWordlePage extends ConsumerStatefulWidget {
   const GameWordlePage({super.key});
