12a394a78e8927c1d84fc23d835efa7267b7d250
Liplum <Li_plum@outlook.com>
2024-05-23 03:57:17 +0800

    [tools] scripts get rid of env var

--- tools/bot-notify-build.mjs
+++ tools/bot-notify-build.mjs
@@ -1,10 +1,32 @@
 import esMain from "es-main"
 import { sendMessageToQQGroup } from "./bot.mjs"
+import { app } from '@liplum/cli'
 
 async function main() {
-  const botUrl = process.env.BOT_URL
-  const version = process.env.LATEST_VERSION
-  const groupNumber = process.argv[2]
+  const args = app({
+    name: 'bot-notify-build',
+    description: 'Bot sends a notification about the latest building in QQ group.',
+    examples: ['node ./bot-notify-build.mjs -u <url> -g <number> -v <version>',],
+    require: ["url", 'group_number', 'version'],
+    options: [{
+      name: 'url',
+      alias: "u",
+      defaultOption: true,
+      description: 'The bot url.'
+    }, {
+      name: 'group_number',
+      alias: "g",
+      description: 'The number of QQ group to send notifications.'
+    }, {
+      name: 'version',
+      alias: "v",
+      description: 'The version of the latest building.'
+    },],
+  })
+
+  const botUrl = args.url
+  const version = args.version
+  const groupNumber = args.group_number
   const message = `最新测试版${version}已完成构建，Android用户可在群公告中访问最新预览版下载地址；请iOS用户等待发布TestFlight测试`
   const result = await sendMessageToQQGroup({ botUrl, groupNumber, message })
   console.log(result)
--- tools/upload-sitmc.mjs
+++ tools/upload-sitmc.mjs
@@ -6,25 +6,26 @@ import esMain from "es-main"
 async function main() {
   const args = app({
     name: 'upload-sitmc',
-    description: 'Upload files onto SIT-MC server. Specify the $SITMC_TEMP_SERVER_AUTH in your environment variables.',
-    examples: ['node ./upload-sitmc.mjs -s <file> -d <path>',],
-    require: ['source'],
-    options: [
-      {
-        name: 'source',
-        alias: "s",
-        defaultOption: true,
-        description: 'The path of local file to upload to SIT-MC server.'
-      },
-      {
-        name: 'destination',
-        alias: "d",
-        description: 'The server path where to save the uploaded file.'
-      },
-    ],
+    description: 'Upload files onto SIT-MC server.',
+    examples: ['node ./upload-sitmc.mjs -k <auth> -s <file> -d <path>',],
+    require: ["auth", 'source'],
+    options: [{
+      name: 'auth',
+      alias: "k",
+      description: 'The authentication key of SIT-MC server.'
+    }, {
+      name: 'source',
+      alias: "s",
+      defaultOption: true,
+      description: 'The path of local file to upload to SIT-MC server.'
+    }, {
+      name: 'destination',
+      alias: "d",
+      description: 'The server path where to save the uploaded file.'
+    },],
   })
 
-  const auth = process.env.SITMC_TEMP_SERVER_AUTH
+  const auth = args.auth
   const filePath = args.source
   const remotePath = args.destination ?? path.join(path.basename(path.dirname(filePath)), path.basename(filePath))
   const res = await uploadFile({
