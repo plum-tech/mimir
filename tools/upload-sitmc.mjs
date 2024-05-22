fccd882004b3c6356f7b108f718c66c5bc257fd6
Liplum <Li_plum@outlook.com>
2024-05-23 01:03:57 +0800

    [tools] new command line args package

--- tools/upload-sitmc.mjs
+++ tools/upload-sitmc.mjs
@@ -1,10 +1,35 @@
 import { uploadFile } from "./sitmc.mjs"
 import * as path from "path"
+import { app } from 'command-line-application'
+import esMain from "es-main"
+
 
 async function main() {
+  const args = app({
+    name: 'upload-sitmc',
+    description: 'Upload files onto SIT-MC server',
+    examples: ['node ./upload-sitmc.mjs -s <file> -d <path>',],
+    require: ['source'],
+    options: [
+      {
+        name: 'source',
+        type: String,
+        alias: "s",
+        defaultOption: true,
+        description: 'The path of local file to upload to SIT-MC server.'
+      },
+      {
+        name: 'destination',
+        type: String,
+        alias: "d",
+        description: 'The server path where to save the uploaded file.'
+      },
+    ],
+  })
+
   const auth = process.env.SITMC_TEMP_SERVER_AUTH
-  const filePath = process.argv[2]
-  const remotePath = process.argv[3] ?? path.join(path.basename(path.dirname(filePath)), path.basename(filePath))
+  const filePath = args.source
+  const remotePath = args.destination ?? path.join(path.basename(path.dirname(filePath)), path.basename(filePath))
   const res = await uploadFile({
     auth,
     localFilePath: filePath,
@@ -13,4 +38,7 @@ async function main() {
   console.log(res)
 }
 
-main()
+
+if (esMain(import.meta)) {
+  main()
+}
