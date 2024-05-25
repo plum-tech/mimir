3691e8be4b41d34d1998525645e7df14997002ed
Liplum <Li_plum@outlook.com>
2024-05-26 03:11:18 +0800

    [network] debug cookies operation

--- lib/storage/hive/cookie.dart
+++ lib/storage/hive/cookie.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+import 'package:sit/r.dart';
 import 'package:sit/utils/hive.dart';
 import 'package:cookie_jar/cookie_jar.dart';
 import 'package:hive/hive.dart';
@@ -8,17 +10,45 @@ class HiveCookieJar implements Storage {
   const HiveCookieJar(this.box);
 
   @override
-  Future<void> init(bool persistSession, bool ignoreExpires) async {}
+  Future<void> init(bool persistSession, bool ignoreExpires) async {
+    if (R.debugNetwork) {
+      debugPrint("[$HiveCookieJar] persistSession=$persistSession, ignoreExpires=$ignoreExpires");
+    }
+  }
 
   @override
-  Future<void> write(String key, String value) async => box.safePut<String>(key, value);
+  Future<void> write(String key, String value) async {
+    if (R.debugNetwork) {
+      debugPrint("[$HiveCookieJar] Writing cookie: $key=$value.");
+    }
+    await box.safePut<String>(key, value);
+  }
 
   @override
-  Future<String?> read(String key) async => box.safeGet<String>(key);
+  Future<String?> read(String key) async {
+    if (R.debugNetwork) {
+      debugPrint("[$HiveCookieJar] Reading cookie: $key.");
+    }
+    final value = box.safeGet<String?>(key);
+    if (R.debugNetwork) {
+      debugPrint("[$HiveCookieJar] Read cookie: $key=$value.");
+    }
+    return value;
+  }
 
   @override
-  Future<void> delete(String key) async => box.delete(key);
+  Future<void> delete(String key) async {
+    if (R.debugNetwork) {
+      debugPrint("[$HiveCookieJar] Deleting cookie: $key.");
+    }
+    await box.delete(key);
+  }
 
   @override
-  Future<void> deleteAll(List<String> keys) async => box.deleteAll(keys);
+  Future<void> deleteAll(List<String> keys) async {
+    if (R.debugNetwork) {
+      debugPrint("[$HiveCookieJar] Deleting all cookies: $keys.");
+    }
+    await box.deleteAll(keys);
+  }
 }
