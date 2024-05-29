1aa37eeac54b1c975c8b5f8f3eb60c6f49c7095e
Liplum <Li_plum@outlook.com>
2024-05-29 19:22:26 +0800

    [tools] set github context

--- tools/lib/github.mjs
+++ tools/lib/github.mjs
@@ -1,7 +1,21 @@
 import { context } from "@actions/github"
 
 export { context } from "@actions/github"
-export const github = context.payload
+export var github = context.payload
+
+export const setGitHub = (ctx) => {
+  github = ctx
+}
+
+export const setGithubFromUrl = async (url) => {
+  const res = await fetch(url)
+  const payload = await res.json()
+  setGitHub(payload)
+}
+
+export const getLatestReleaseApiUrl = (repo) => {
+  return `https://api.github.com/repos/${repo}/releases/latest`
+}
 
 export const getGitHubMirrorDownloadUrl = (url) => {
   return `https://mirror.ghproxy.com/${url}`
