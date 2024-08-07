66a7639f3be4ca388ebeb7c178ea7985d3332e9f
Liplum <Li_plum@outlook.com>
2024-08-07 16:52:17 +0800

    bump flutter and packages to the latest

--- lib/school/exam_result/index.pg.dart
+++ lib/school/exam_result/index.pg.dart
@@ -1,4 +1,4 @@
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:carousel_slider_plus/carousel_slider_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
--- lib/timetable/p13n/page/palette.dart
+++ lib/timetable/p13n/page/palette.dart
@@ -1,4 +1,4 @@
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:carousel_slider_plus/carousel_slider_plus.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
