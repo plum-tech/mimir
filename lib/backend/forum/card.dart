012054ba738e1c6dee46b7f0cb5c2d396168fb2f
Liplum <Li_plum@outlook.com>
2024-08-26 22:45:48 +0800

    [forum] coming soon

--- lib/backend/forum/card.dart
+++ lib/backend/forum/card.dart
@@ -7,6 +7,7 @@ import 'package:sit/backend/init.dart';
 import 'package:sit/design/adaptive/multiplatform.dart';
 import 'package:sit/design/widgets/app.dart';
 import 'package:sit/l10n/extension.dart';
+import 'package:sit/settings/dev.dart';
 import 'package:sit/utils/guard_launch.dart';
 import 'package:text_scroll/text_scroll.dart';
 import 'package:universal_platform/universal_platform.dart';
@@ -41,19 +42,24 @@ class _ForumAppCardState extends ConsumerState<ForumAppCard> {
   @override
   Widget build(BuildContext context) {
     final bulletin = this.bulletin;
+    final dev = ref.watch(Dev.$on);
     return AppCard(
       title: "小应社区".text(),
       view: bulletin == null ? null : buildBulletin(bulletin),
       leftActions: [
         FilledButton.icon(
-          onPressed: () async {
-            if (UniversalPlatform.isAndroid || UniversalPlatform.isIOS) {
-              await context.push("/forum");
-            } else {
-              await guardLaunchUrl(context, R.forumUri);
-            }
-          },
-          label: "进入".text(),
+          onPressed: !dev
+              ? null
+              : () async {
+                  if (UniversalPlatform.isAndroid || UniversalPlatform.isIOS) {
+                    await context.push("/forum");
+                  } else {
+                    await guardLaunchUrl(context, R.forumUri);
+                  }
+                },
+          label: !dev
+              ? "敬请期待".text()
+              : "进入".text(),
           icon: Icon(context.icons.home),
         ),
       ],
