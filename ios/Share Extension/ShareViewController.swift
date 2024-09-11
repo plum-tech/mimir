06aa9038c325c90aa07260f797b79dd5b7498482
Liplum <Li_plum@outlook.com>
2024-09-11 22:06:02 +0800

    added Share Extension

---
+++ ios/Share Extension/ShareViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ShareViewController.swift
+//  Share Extension
+//
+//  Created by Liplum on 9/11/24.
+//
+
+import UIKit
+import Social
+
+// If you get no such module 'receive_sharing_intent' error.
+// Go to Build Phases of your Runner target and
+// move `Embed Foundation Extension` to the top of `Thin Binary`.
+import receive_sharing_intent
+
+class ShareViewController: RSIShareViewController {
+
+  // Use this method to return false if you don't want to redirect to host app automatically.
+  // Default is true
+  override func shouldAutoRedirect() -> Bool {
+    return false
+  }
+}
