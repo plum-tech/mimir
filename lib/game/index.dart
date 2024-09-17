3d5572211ce011b149e7b0bfb06286bc2828b6b0
Liplum <Li_plum@outlook.com>
2024-09-17 17:35:34 +0800

    [game] CN bureau advice for games

--- lib/game/index.dart
+++ lib/game/index.dart
@@ -1,5 +1,7 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mimir/r.dart';
 import 'package:rettulf/rettulf.dart';
 import 'package:mimir/game/2048/card.dart';
 import 'package:mimir/game/minesweeper/card.dart';
@@ -49,11 +51,41 @@ class _GamePageState extends ConsumerState<GamePage> {
                     name: "SIT Suika",
                     baseRoute: "/suika",
                   ),
+                if (context.locale == R.zhHansLocale)
+                  const Opacity(
+                    opacity: 0.75,
+                    child: CnBureauAdvice4Game(),
+                  ).padSymmetric(h: 8, v: 12),
               ],
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+const bureauAdvice = """
+抵制不良游戏，拒绝盗版游戏。
+
+注意自我保护，谨防受骗上当。
+
+适度游戏益脑，沉迷游戏伤身。
+
+合理安排时间，享受健康生活。
+""";
+
+class CnBureauAdvice4Game extends StatelessWidget {
+  const CnBureauAdvice4Game({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget title(String text) => text.text(textAlign: TextAlign.center);
+    Widget sub(String text) => text.text(style: context.textTheme.labelMedium, textAlign: TextAlign.center);
+    return [
+      title("健康游戏忠告"),
+      sub("抵制不良游戏，拒绝盗版游戏。注意自我保护，谨防受骗上当。"),
+      sub("适度游戏益脑，沉迷游戏伤身。合理安排时间，享受健康生活。"),
+    ].column();
   }
 }
